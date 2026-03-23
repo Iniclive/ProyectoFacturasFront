c1929a166f04235a65926cd10e4889ba850e984d
Iniclive <iniclive@gmail.com>
2026-03-23 14:55:51 +0100

    Quitamos console log de debug

--- src/app/core/guards/no-auth.guard.ts
+++ src/app/core/guards/no-auth.guard.ts
@@ -6,12 +6,8 @@ import { map } from "rxjs";
 export const noAuthGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
-
-  console.log('🛡️ noAuthGuard ejecutado - isAuthenticated:', authService.isAuthenticated());
-
   return authService.verifySession().pipe(
     map(authenticated => {
-      console.log('🛡️ noAuthGuard resultado:', authenticated);
       return authenticated ? router.createUrlTree(['/facturas']) : true;
     })
   );
